aa6048624c225011190f28e40c38b9bc0c7b9a14
Huy Ngo <huy.ngoduc@hotmail.com>
2022-09-07 00:22:09 +0700

    commit missing file

---
+++ Service.Contracts/ICompanyService.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Model;
+using Service.Contracts.DTO;
+
+namespace Service.Contracts;
+
+public interface ICompanyService
+{
+    IEnumerable<CompanyDto> GetAllCompanies(bool trackingChanges);
+    CompanyDto GetCompany(Guid id, bool trackingChanges);
+}
---
+++ Service/CompanyService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.Exceptions;
+using Entities.Model;
+using Microsoft.Extensions.Logging;
+using Service.Contracts;
+using Service.Contracts.DTO;
+
+namespace Service;
+
+public sealed class CompanyService : ICompanyService
+{
+    private readonly ILogger<CompanyService> _logger;
+    private readonly ICompanyRepository _repository;
+    private readonly IMapper _mapper;
+
+    public CompanyService(ILogger<CompanyService> logger, ICompanyRepository repository, IMapper mapper)
+    {
+        _logger = logger;
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public IEnumerable<CompanyDto> GetAllCompanies(bool trackingChanges)
+    {
+        try
+        {
+            var companies = _repository.FindAll(trackingChanges)
+                .OrderBy(x => x.Name)
+                .ToList();
+
+            return _mapper.Map<IEnumerable<CompanyDto>>(companies);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Something went wrong");
+            throw;
+        }
+    }
+
+    public CompanyDto GetCompany(Guid id, bool trackingChanges)
+    {
+        var company = _repository.FindByCondition(c => c.Id.Equals(id), trackingChanges)
+            .SingleOrDefault();
+
+        if (company == null)
+        {
+            throw new CompanyNotFoundException(id);
+        }
+        return _mapper.Map<CompanyDto>(company);
+    }
+}
